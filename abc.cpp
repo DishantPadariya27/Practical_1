684a745f2241dcdbe9734da96e8d81d7c20aad5d
DishantPadariya27 <21bce174@nirmauni.ac.in>
2024-07-26 17:20:35 +0530

    codeforces

---
+++ abc.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll          long long int
+#define f(i,x,n)    for(ll i=x;i<n;i++)
+#define all(v) v.begin(),v.end()
+#define read(v,n) for(int i=0;i<n;i++) cin>>v[i];
+#define lcm(a,b)  (a*b)/__gcd(a,b);
+#define cy   cout<<"YES"<<endl
+#define cn   cout<<"NO"<<endl
+#define Speed  ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+
+
+void solve() {
+
+        
+}      
+
+int main() {
+ 
+    Speed
+    ll t=1;
+    // cin>>t;
+    while(t--) {
+
+        solve();
+
+    } 
+}
